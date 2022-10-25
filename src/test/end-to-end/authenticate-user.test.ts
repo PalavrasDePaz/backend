2bcf6523e024640180e099fc48ecaa67087580f5
JVSRamos <joaoramosj@usp.br>
2022-10-25 16:36:10 -0300

    test: fix authentication test
    co-authored-by: alaurai <alauraivani@gmail.com>

--- src/test/end-to-end/authenticate-user.test.ts
+++ src/test/end-to-end/authenticate-user.test.ts
@@ -2,8 +2,9 @@ import request from 'supertest';
 import server from '@src/server';
 
 describe('User Session', () => {
-  it('Should return error 401 no authentication token presented', async () => {
+  it('Should return error 400 no header provided', async () => {
     const respose = await request(server).get('/');
-    expect(respose.statusCode).toBe(401);
+    expect(respose.statusCode).toBe(400);
+    expect(respose.text).toBe('Nenhum header encontrado!');
   });
 });
