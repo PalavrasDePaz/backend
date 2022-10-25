29cd522dacc0c116f1a3fc1415d35b48f3f5bca6
JVSRamos <joaoramosj@usp.br>
2022-10-25 16:35:07 -0300

    test: fix volunteer controller test
    co-authored-by: alaurai <alauraivani@gmail.com>

--- src/test/controllers/sequelize-volunteer-controller.test.ts
+++ src/test/controllers/sequelize-volunteer-controller.test.ts
@@ -1,7 +1,8 @@
-import { VolunteerController } from '@src/controllers/volunteer-controller';
+import { SequelizeVolunteerController } from '@src/controllers/implementations/sequelize-volunteer-controller';
+import { VolunteerController } from '@src/controllers/interfaces/volunteer-controller';
 import { VolunteerEntity } from '@src/entities/volunteer-entity';
 import initModels from '@src/services/database';
-import { Volunteer } from '@src/services/database/models/volunteer';
+import sequelize from '@src/services/database/sequelize';
 import volunteerDummy from '../dummies/volunteer-dummy';
 
 describe('Volunteer Controller', () => {
@@ -9,40 +10,53 @@ describe('Volunteer Controller', () => {
 
   beforeAll(() => {
     initModels();
-    volunteerController = new VolunteerController();
+    volunteerController = new SequelizeVolunteerController();
+  });
+
+  afterAll(() => {
+    sequelize.close();
   });
 
   it('Should get a volunteer by the email', async () => {
     const volunteer: VolunteerEntity = volunteerDummy;
+
+    await volunteerController.createVolunteer(volunteer);
 
     const result = await volunteerController.getVolunteerByEmail(
       volunteer.email
     );
 
     expect(result).toEqual(volunteer);
+    await volunteerController.deleteVolunteerByEmail(volunteer.email);
   });
 
   it('Should create a new volunteer', async () => {
     const volunteer: VolunteerEntity = volunteerDummy;
 
     const result = await volunteerController.createVolunteer(volunteer);
-    const searched = await VolunteerController.getVolunteerByEmail(volunteer);
+    const searched = await volunteerController.getVolunteerByEmail(
+      volunteer.email
+    );
 
     expect(result).toEqual(volunteer);
     expect(searched).toEqual(volunteer);
 
-    await volunteerController.deleteVolunteer(volunteer);
+    await volunteerController.deleteVolunteerByEmail(volunteer.email);
   });
 
   it('Should delete a volunteer', async () => {
     const volunteer: VolunteerEntity = volunteerDummy;
     await volunteerController.createVolunteer(volunteer);
 
-    const result = await volunteerController.deleteVolunteer(volunteer);
-
-    expect(result).toEqual(volunteer);
-    expect(await VolunteerController.getVolunteerByEmail(volunteer)).toThrow(
-      VolunteerNotFoundError
+    const numDeletedRows = await volunteerController.deleteVolunteerByEmail(
+      volunteer.email
     );
+
+    expect(numDeletedRows).toBe(1);
+
+    const result = await volunteerController.getVolunteerByEmail(
+      volunteer.email
+    );
+    expect(result).toBeNull();
   });
 });
