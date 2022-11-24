e1f157d33042a066d059984a3c810ce71b0c8fdc
JVSRamos <joaoramosj@usp.br>
2022-11-23 22:06:03 -0300

    test: remove session test

--- src/test/controllers/session-controller.test.ts
+++
@@ -1,9 +0,0 @@
-describe('Session Controller', () => {
-  it('Should verify that the user exists', () => {
-    return;
-  });
-
-  it('Should create a user that does not exists in the database', () => {
-    return;
-  });
-});
