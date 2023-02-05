56457e136da8ce704439127f1aad4961ed2d29f1
JVSRamos <joaoramosj@usp.br>
2023-02-05 01:16:53 -0300

    fix: fix start:dev and build and remove firebase

--- src/services/firebase/index.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-
-admin.initializeApp({
-  credential: admin.credential.cert(
-    './src/services/firebase/firebase-admin-credential.json'
-  )
-});
-
-export default admin;
