915826114c98ad934b2f4ec08dd082e3758429eb
JVSRamos <joaoramosj@usp.br>
2023-03-06 21:30:51 -0300

    refactor: update api and router for new repository

--- src/presentation/api/volunteer/index.ts
+++ src/presentation/api/volunteer/index.ts
@@ -9,22 +9,39 @@ export class VolunteerAPI {
     this.volunteerRepository = volunteerRepository;
   }
 
-  async createVolunteer(request: Request, response: Response) {
+  updateVolunteer = async (request: Request, response: Response) => {
     try {
-      const volunteer: VolunteerEntity = request.body.volunteer;
+      const volunteer: VolunteerEntity = request.body;
+      const successUpdate = await this.volunteerRepository.updateVolunteer(
+        volunteer.email,
+        volunteer
+      );
+
+      successUpdate
+        ? response.status(200).send({ volunteer })
+        : response.status(400).send({ error: 'Volunteer not updated' });
+    } catch (error) {
+      response.status(400).send({ error: error });
+    }
+  };
+
+  createVolunteer = async (request: Request, response: Response) => {
+    try {
+      const volunteer: VolunteerEntity = request.body;
       const createdVolunteer = await this.volunteerRepository.createVolunteer(
         volunteer
       );
+
       response.status(201).send({ volunteer: createdVolunteer });
     } catch (error) {
       response.status(400).send({ error: error });
     }
-  }
+  };
 
-  async getAllVolunteers(_: Request, response: Response) {
+  getAllVolunteers = async (_: Request, response: Response) => {
     const volunteers = await this.volunteerRepository.getAllVolunteers();
     response.status(200).send({ volunteers: volunteers });
-  }
+  };
 
   getVolunteerByEmail = async (request: Request, response: Response) => {
     const email = request.params.email;
--- src/presentation/routers/volunteer/index.ts
+++ src/presentation/routers/volunteer/index.ts
@@ -3,9 +3,9 @@ import { VolunteerAPI } from '@src/presentation/api/volunteer';
 
 export default function volunteerRoutes(api: VolunteerAPI): Router {
   const router = Router();
+  router.post('/update', api.updateVolunteer);
   router.post('/create', api.createVolunteer);
   router.get('/', api.getAllVolunteers);
   router.get('/:email', api.getVolunteerByEmail);
-
   return router;
 }
