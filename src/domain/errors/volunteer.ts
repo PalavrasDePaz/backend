cb92a75d3a389cf18eb4a9cff4ba213344ab040d
JVSRamos <joaoramosj@usp.br>
2023-04-30 11:00:32 -0300

    feat: add more error names to error types

--- src/domain/errors/volunteer.ts
+++ src/domain/errors/volunteer.ts
@@ -3,6 +3,7 @@ import { ErrorBase } from '@src/helpers/error-base';
 type ErrorName =
   | 'VOLUNTEER_NOT_FOUND'
   | 'VOLUNTEER_ALREADY_EXISTS'
-  | 'VOLUNTEER_NOT_UPDATED';
+  | 'VOLUNTEER_NOT_UPDATED'
+  | 'VOLUNTEER_NOT_DELETED';
 
 export class VolunteerError extends ErrorBase<ErrorName> {}
