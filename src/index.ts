fcf969cf86c2b3ccdc6b2757296a686965b3669e
alaurai <alauraivani@gmail.com>
2023-05-11 21:14:16 -0300

    ci: config server port

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import 'module-alias/register';
 import { app } from './app';
-
-const PORT = 3001;
-
+import { PORT } from '@src/config/server';
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
