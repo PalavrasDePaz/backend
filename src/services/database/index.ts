24940b7d4e5952dfa76145e57740f26f7626f0cd
JVSRamos <joaoramosj@usp.br>
2023-06-16 18:00:57 -0300

    feat: start adding relationship in models

--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -1,3 +1,4 @@
+import { Pep } from './models/class';
 import { Notebooks } from './models/notebooks';
 import { Volunteer } from './models/volunteer';
 import sequelize from './sequelize';
@@ -5,6 +6,10 @@ import sequelize from './sequelize';
 const initModels = () => {
   Volunteer.initialize(sequelize);
   Notebooks.initialize(sequelize);
+  Pep.initialize(sequelize);
+
+  Pep.hasMany(Notebooks, { foreignKey: 'idpep' });
+  Notebooks.belongsTo(Pep);
 };
 
 export default initModels;
