958460e2707aa22cb2e3c4cf8dcaccb36c550e00
JVSRamos <joaoramosj@usp.br>
2023-06-24 14:35:00 -0300

    fix: add missing fields to notebook row type

--- src/domain/entities/available-notebook-row-entity.ts
+++ src/domain/entities/available-notebook-row-entity.ts
@@ -8,6 +8,8 @@
  */
 export type AvailableNotebookRowEntity = {
   notebookId: number;
+  studentId: number;
+  idPep?: number;
   studentName: string;
   reservationDate?: Date | null;
   notebookPath?: string | null;
