b9c0c1bf2cff69596fa01bf37fa86c4a2e010f59
JVSRamos <joaoramosj@usp.br>
2023-08-08 22:11:19 -0300

    feat: add logger config

---
+++ src/services/logger/logger.ts
@@ -0,0 +1,27 @@
+import { createLogger, format, transports } from 'winston';
+
+export const logger = createLogger({
+  format: format.combine(
+    format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.json()
+  ),
+  transports: [
+    new transports.File({ filename: 'error.log', level: 'error' }),
+    new transports.File({ filename: 'combined.log' })
+  ],
+  exceptionHandlers: [
+    new transports.File({ filename: 'error.log', level: 'error' })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.combine(format.colorize(), format.simple())
+    })
+  );
+}
