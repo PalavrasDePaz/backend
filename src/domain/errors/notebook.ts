ae0eeeac1861830a470329f7b6c4feff0632f51c
JVSRamos <joaoramosj@usp.br>
2023-08-11 19:10:14 -0300

    feat: add download notebook route

--- src/domain/errors/notebook.ts
+++ src/domain/errors/notebook.ts
@@ -3,6 +3,7 @@ import { ErrorBase } from '@src/helpers/error-base';
 type ErrorName =
   | 'NOTEBOOK_NOT_FOUND'
   | 'NOTEBOOK_ALREADY_RESERVED_ERROR'
-  | 'NOTEBOOK_ALREADY_EVALUATED_ERROR';
+  | 'NOTEBOOK_ALREADY_EVALUATED_ERROR'
+  | 'NOTEBOOK_DIRECTORY_NOT_FOUND_ERROR';
 
 export class NotebookError extends ErrorBase<ErrorName> {}
