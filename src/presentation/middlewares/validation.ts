48313898de4522705152c462c299e2fba498e151
JVSRamos <joaoramosj@usp.br>
2023-08-11 10:33:49 -0300

    fix: fix lint issues

--- src/presentation/middlewares/validation.ts
+++ src/presentation/middlewares/validation.ts
@@ -13,7 +13,6 @@ export function validationMiddleware(
   res: Response,
   next: NextFunction
 ): Response | void {
-  console.error(err);
   if (err instanceof ValidateError) {
     // eslint-disable-next-line no-console
     console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
