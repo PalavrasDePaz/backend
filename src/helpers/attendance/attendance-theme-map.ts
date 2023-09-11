c72ecf393c971bfe4dda87ce1f0803636eea4bc4
JVSRamos <joaoramosj@usp.br>
2023-09-11 17:16:38 -0300

    feat: add attendance metrics route

---
+++ src/helpers/attendance/attendance-theme-map.ts
@@ -0,0 +1,29 @@
+const attendanceThemeMap: Record<string, string> = {
+  '1 - PAZ': '`1`',
+  '2 - APRECIAÇÃO': '`2`',
+  '3 - FORÇA INTERIOR': '`3`',
+  '4 - CONSCIÊNCIA DE SI MESMO': '`4`',
+  '5 - CLAREZA': '`5`',
+  '6 - ENTENDIMENTO': '`6`',
+  '7 - DIGNIDADE': '`7`',
+  '8 - ESCOLHA': '`8`',
+  '9 - ESPERANÇA': '`9`',
+  '10 - CONTENTAMENTO': '`10`',
+  'ENCONTRO COM VOLUNTÁRIOS': '`11`',
+  'LEITURA LIVRO - INTRODUÇÂO': 'Livro 0',
+  'LEITURA LIVRO - CAPITULO 1': 'Livro 1',
+  'LEITURA LIVRO - CAPITULO 2': 'Livro 2',
+  'LEITURA LIVRO - CAPITULO 3': 'Livro 3',
+  'LEITURA LIVRO - CAPITULO 4': 'Livro 4',
+  'LEITURA LIVRO - CAPITULO 5': 'Livro 5',
+  'LEITURA LIVRO - CAPITULO 6': 'Livro 6',
+  'LEITURA LIVRO - CAPITULO 7': 'Livro 7',
+  'LEITURA LIVRO - CAPITULO 8': 'Livro 8',
+  'LEITURA LIVRO - CAPITULO 9': 'Livro 9',
+  'LEITURA LIVRO - CAPITULO 10': 'Livro 10',
+  'LEITURA LIVRO - CAPITULO 11': 'Livro 11',
+  'LEITURA LIVRO - CAPITULO 12': 'Livro 12',
+  'ORIENTAÇÔES PARA FACILITAÇÂO': 'Facil'
+};
+
+export default attendanceThemeMap;
