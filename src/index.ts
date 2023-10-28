d93cf767f5d2210774dcab8b9f18d50c3f874f4c
JVSRamos <joaoramosj@usp.br>
2023-10-28 11:13:28 -0300

    feat: add https config for server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import 'module-alias/register';
 import { app } from './app';
-import { PORT } from '@src/config/server';
-app.listen(PORT, () => {
+import { NODE_ENV, PORT } from '@src/config/server';
+import { readFileSync } from 'fs';
+import { createServer } from 'https';
+
+if (NODE_ENV == 'production') {
+  const server_key = readFileSync('../server.key');
+  const server_cert = readFileSync('../server.crt');
+
+  const server = createServer({ key: server_key, cert: server_cert }, app);
+
+  server.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`[server]: Server is running on port ${PORT}`);
+  });
+} else if (NODE_ENV == 'development') {
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`[server]: Server is running on port ${PORT}`);
+  });
+} else {
   // eslint-disable-next-line no-console
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+  console.log(`[server]: Configuration ${NODE_ENV} not available`);
+}
