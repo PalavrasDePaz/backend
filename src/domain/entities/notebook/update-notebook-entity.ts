f403410f364c584d6399bc71f540f8aed3faa3a4
Victoria Teixeira <97026355+victoriateixeira@users.noreply.github.com>
2023-10-31 15:13:32 -0300

    Update update-notebook-entity.ts
    fix: fixes multi-line bug in update-notebook-entity

--- src/domain/entities/notebook/update-notebook-entity.ts
+++ src/domain/entities/notebook/update-notebook-entity.ts
@@ -1,14 +1,5 @@
 import { NotebookEntity } from './notebook-entity';
 
 export type UpdateNotebookEntity = Omit<
-  NotebookEntity,
-  | 'idcad'
-  | 'idvol'
-  | 'idpep'
-  | 'studentName'
-  | 'studentRegistration'
-  | 'studentPrisonUnit'
-  | 'evaluatorName'
-  | 'evaluatorEmail'
-  | 'notebookDirectory'
+  NotebookEntity, 'idcad' | 'idvol' | 'idpep' | 'studentName' | 'studentRegistration' | 'studentPrisonUnit' | 'evaluatorName' | 'evaluatorEmail' | 'notebookDirectory'
 >;
