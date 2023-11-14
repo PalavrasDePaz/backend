c72287dd479063efac094550cfe46a9fc11a793d
JVSRamos <joaoramosj@usp.br>
2023-11-14 08:35:21 -0300

    fix: delete testes folder

--- src/test/dummies/create-volunteer-entity-dummy.ts
+++
@@ -1,8 +0,0 @@
-import { CreateVolunteerEntity } from '@src/domain/entities/volunteer/create-volunteer-entity';
-import volunteerWithAuthEntityDummy from './volunteer-with-auth-entity-dummy';
-
-const createVolunteerEntityDummy: CreateVolunteerEntity = {
-  ...volunteerWithAuthEntityDummy
-};
-
-export default createVolunteerEntityDummy;
