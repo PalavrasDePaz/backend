84294a877f76c748f678ef7214c3a379b50b0f23
gonzagadavid <david.blesses@hotmail.com>
2023-11-30 14:33:13 -0300

    feat: create attendance from download mapper

--- src/domain/entities/attendance/attendence-dowload-info-entity.ts
+++ src/domain/entities/attendance/attendence-dowload-info-entity.ts
@@ -1,3 +1,13 @@
-import { Attendance } from '@src/services/database/models/attendance';
-
-export type AttendanceDownloadInfoEntity = Attendance;
+export type AttendanceDownloadInfoEntity = {
+  'Data de Submissão': Date;
+  'ID Voluntário': number;
+  Nome: string;
+  'Assunto do Workshop': string;
+  'Desafio Enfrentado': string;
+  'Conhecimentos adquiridos': string;
+  Aprendizado: string;
+  'Força Interior': string;
+  'Tempo Suficiente': string;
+  'Retenção no Estudo': string;
+  'Feedback sobre a Melhoria': string;
+};
