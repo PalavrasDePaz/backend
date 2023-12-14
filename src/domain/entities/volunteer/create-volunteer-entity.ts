62c3722ba97e05db24a4a7e46bcd44260f5c4037
Gonzagadavid <gonzagadaviddev@gmail.com>
2023-12-13 21:04:27 -0300

    fix: remove unused fields from createVolunteer

--- src/domain/entities/volunteer/create-volunteer-entity.ts
+++ src/domain/entities/volunteer/create-volunteer-entity.ts
@@ -2,5 +2,5 @@ import { VolunteerWithAuthEntity } from './volunteer-with-auth-entity';
 
 export type CreateVolunteerEntity = Omit<
   VolunteerWithAuthEntity,
-  'idvol' | 'createdAt'
+  'idvol' | 'createdAt' | 'isDisability' | 'opportunities'  | 'readSkill' | 'bookSkill' | 'authorization' | 'certificate' | 'courseOne' | 'courseTwo'
 >;
