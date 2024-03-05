f0cbc4c72f56b855ab1c3cd35c621037bc53c391
Danrllei Miranda <danmiranda14@gmail.com>
2024-03-05 18:33:45 -0300

    feat: pagination route GET/ pep-with-place-fullname

--- src/domain/interfaces/repositories/pep-class-repository.ts
+++ src/domain/interfaces/repositories/pep-class-repository.ts
@@ -1,6 +1,10 @@
-import { PepClassEntity } from '@src/domain/entities/pep-class/pep-class-entity';
+import {
+  PepClassEntity,
+  PepClassWithPlace
+} from '@src/domain/entities/pep-class/pep-class-entity';
 import { UpdatePepClassEntity } from '@src/domain/entities/pep-class/update-pep-class-entity';
-
+import { PaginationParams } from '@src/presentation/types/paginationParams';
+import { PaginationResult } from '@src/services/repositories/helpers/wrapPagination';
 export interface PepClassRepository {
   getClassesFromId(classId: number): Promise<PepClassEntity[]>;
 
@@ -10,4 +14,8 @@ export interface PepClassRepository {
     classId: number,
     pepClass: UpdatePepClassEntity
   ): Promise<PepClassEntity | null>;
+
+  getAllPepClasses(
+    pagination: PaginationParams
+  ): Promise<PaginationResult<PepClassWithPlace[]>>;
 }
--- src/services/database/mappers/pep-class.ts
+++ src/services/database/mappers/pep-class.ts
@@ -1,6 +1,9 @@
-import { PepClassEntity } from '@src/domain/entities/pep-class/pep-class-entity';
-import { Pep } from '../models/class';
+import {
+  PepClassEntity,
+  PepClassWithPlace
+} from '@src/domain/entities/pep-class/pep-class-entity';
 import { UpdatePepClassEntity } from '@src/domain/entities/pep-class/update-pep-class-entity';
+import { Pep } from '../models/class';
 import UpdateModel from './helpers/update-model-type';
 
 export const pepClassModelToEntity = (pepClass: Pep): PepClassEntity => {
@@ -19,6 +22,20 @@ export const pepClassModelToEntity = (pepClass: Pep): PepClassEntity => {
     notebookDirectory: pepClass.directory
   };
 };
+
+export const pepClassPlaceModelToEntity = (
+  pepClass: Pep & { 'place.fullName'?: string }
+): PepClassWithPlace => ({
+  id: pepClass.id,
+  placeId: pepClass.idPlace,
+  fullName: pepClass?.['place.fullName'] ?? '',
+  groupName: pepClass.nGroup,
+  classOneDate: pepClass.t1,
+  classTenDate: pepClass.t10,
+  numEnrolled: pepClass.total,
+  numEnrolledGotCertificate: pepClass['total cert'],
+  notebookDirectory: pepClass.directory
+});
 
 export const updatePepClassEntityToUpdateModel = (
   pepClass: UpdatePepClassEntity
