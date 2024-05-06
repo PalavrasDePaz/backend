6b90a7b6a93eaa0001a62b826a1a13bb4517c6a8
Danrllei Miranda <danmiranda14@gmail.com>
2024-05-06 20:25:02 -0300

    fix(volunteers): fix email to reset password

--- src/services/email-service/use-cases/send-volunteer-create-email.ts
+++ src/services/email-service/use-cases/send-volunteer-create-email.ts
@@ -1,7 +1,7 @@
-import { IEmailManager } from '@src/domain/interfaces/repositories/email-manager';
-import { VolDataForCreatedEmail } from '../types/volunteer-data-for-created-email';
 import { INFO_EMAIL } from '@src/config/server';
+import { IEmailManager } from '@src/domain/interfaces/repositories/email-manager';
 import { getVolCreatedEmailBody } from '@src/helpers/get-vol-created-email-body';
+import { VolDataForCreatedEmail } from '../types/volunteer-data-for-created-email';
 
 export const sendVolunteerCreatedEmail = async (
   emailManager: IEmailManager,
@@ -13,6 +13,13 @@ export const sendVolunteerCreatedEmail = async (
     sender: INFO_EMAIL,
     receiver: volunteerData.email,
     subject: 'Conta criado em Palavras de Paz!',
-    body: body
+    body: body,
+    attachments: [
+      {
+        filename: 'ethicscode.pdf',
+        path: __dirname + '/attachments/ethicscode.pdf',
+        cid: 'uniq-ethicscode.pdf'
+      }
+    ]
   });
 };
