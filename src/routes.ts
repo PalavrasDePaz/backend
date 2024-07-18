cf628983a6a4a8633d35f9bae709a93da344968b
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-07-17 21:55:22 -0300

    feat: create notebooks evaluation download route

--- src/routes.ts
+++ src/routes.ts
@@ -1216,6 +1216,37 @@ export function RegisterRoutes(app: Router) {
             }
         });
         // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+        app.get('/notebooks/evaluation-list/download',
+            authenticateMiddleware([{"jwt":["readPermission"]}]),
+            ...(fetchMiddlewares<RequestHandler>(NotebookAPI)),
+            ...(fetchMiddlewares<RequestHandler>(NotebookAPI.prototype.getNotebooksEvaluationDownload)),
+
+            async function NotebookAPI_getNotebooksEvaluationDownload(request: any, response: any, next: any) {
+            const args = {
+                    req: {"in":"request","name":"req","required":true,"dataType":"object"},
+            };
+
+            // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
+
+            let validatedArgs: any[] = [];
+            try {
+                validatedArgs = getValidatedArgs(args, request, response);
+
+                const container: IocContainer = typeof iocContainer === 'function' ? (iocContainer as IocContainerFactory)(request) : iocContainer;
+
+                const controller: any = await container.get<NotebookAPI>(NotebookAPI);
+                if (typeof controller['setStatus'] === 'function') {
+                controller.setStatus(undefined);
+                }
+
+
+              const promise = controller.getNotebooksEvaluationDownload.apply(controller, validatedArgs as any);
+              promiseHandler(controller, promise, response, 200, next);
+            } catch (err) {
+                return next(err);
+            }
+        });
+        // WARNING: This file was auto-generated with tsoa. Please do not modify it. Re-run tsoa to re-generate this file: https://github.com/lukeautry/tsoa
         app.get('/notebooks/count/:idvol',
             authenticateMiddleware([{"jwt":[]}]),
             ...(fetchMiddlewares<RequestHandler>(NotebookAPI)),
