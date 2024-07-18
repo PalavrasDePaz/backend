3a9b3a92b7669ea3ebf045620b79a90fc5ddfb1d
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-07-17 21:53:51 -0300

    feat: create evalutionListNotebookModelToEntityDownload mapper

--- src/services/database/mappers/notebooks.ts
+++ src/services/database/mappers/notebooks.ts
@@ -1,4 +1,7 @@
-import { EvaluateNotebookEntity } from '@src/domain/entities/notebook/evaluate-notebook-entity';
+import {
+  EvaluateNotebookEntity,
+  EvaluateNotebookEntityDownload
+} from '@src/domain/entities/notebook/evaluate-notebook-entity';
 import { NotebookEntity } from '@src/domain/entities/notebook/notebook-entity';
 import { UpdateNotebookEntity } from '@src/domain/entities/notebook/update-notebook-entity';
 import { Notebook } from '../models/notebook';
@@ -43,6 +46,56 @@ export const notebookModelToEntity = (notebook: Notebook): NotebookEntity => ({
   evaluatedDate: notebook['Carimbo de data/hora'],
   reservationDate: notebook.datareserva,
   notebookDirectory: notebook.pep?.directory
+});
+
+export const evalutionListNotebookModelToEntityDownload = (
+  notebook: Notebook & {
+    'pep.place.fullName'?: string;
+    'volunteer.NOME'?: string;
+  }
+): EvaluateNotebookEntityDownload => ({
+  'ID do caderno': notebook.idcad,
+  'ID do voluntário': notebook.idvol ?? '',
+  'Uni. prisional':
+    notebook['pep.place.fullName'] ||
+    notebook['unidade prisional do(a) aluno(a)'] ||
+    '',
+  'ID da turma': notebook.idpep ?? '',
+  'Nome do voluntário': notebook['volunteer.NOME'] || '',
+  'E-mail do voluntário': notebook.volunteer?.['e-mail'] ?? '',
+  'Matrícula do aluno': notebook['número de matrícula do(a) aluno(a)'] ?? '',
+  'Nome do(a) aluno(a)': notebook['nome do(a) aluno(a)'],
+  'Carimbo de data/hora':
+    notebook['Carimbo de data/hora']?.toLocaleString() ?? '',
+  'Data e hora da reserva': notebook?.datareserva?.toLocaleString() ?? '',
+  'Conteúdos relevantes': notebook['conteúdos relevantes'] ?? '',
+  'Avaliação da Questão 1:': notebook.a1 ?? '',
+  'Avaliação da Questão 2:': notebook.a2 ?? '',
+  'Avaliação da Questão 3:': notebook.a3 ?? '',
+  'Avaliação da Questão 4:': notebook.a4 ?? '',
+  'Avaliação da Questão 5:': notebook.a5 ?? '',
+  'Avaliação da Questão 6:': notebook.a6 ?? '',
+  'Avaliação da Questão 7:': notebook.a7 ?? '',
+  'Avaliação da Questão 8:': notebook.a8 ?? '',
+  'Avaliação da Questão 9:': notebook.a9 ?? '',
+  'Avaliação da Questão 10:': notebook.a10 ?? '',
+  'Avaliação da Questão 11:': notebook.a11 ?? '',
+  'Avaliação da Questão 12:': notebook.a12 ?? '',
+  'Avaliação da Questão 13:': notebook.a13 ?? '',
+  'Questão 1:': notebook['tema 1'] ?? '',
+  'Questão 2:': notebook['tema 2'] ?? '',
+  'Questão 3:': notebook['tema 3'] ?? '',
+  'Questão 4:': notebook['tema 4'] ?? '',
+  'Questão 5:': notebook['tema 5'] ?? '',
+  'Questão 6:': notebook['tema 6'] ?? '',
+  'Questão 7:': notebook['tema 7'] ?? '',
+  'Questão 8:': notebook['tema 8'] ?? '',
+  'Questão 9:': notebook['tema 9'] ?? '',
+  'Questão 10:': notebook['tema 10'] ?? '',
+  'Conclusão do avaliador': notebook['conclusão do avaliador'],
+  Aprovado: notebook.aprovado ?? '',
+  'Exclusão de arquivos recebidos':
+    notebook['exclusão de arquivos recebidos'] ?? ''
 });
 
 export const evalutionListNotebookModelToEntity = (
