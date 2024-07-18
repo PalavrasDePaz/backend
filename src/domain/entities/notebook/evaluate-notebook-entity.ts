30254d6e357180eabd2d54fc6e6f03163c561ed9
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-07-17 21:53:09 -0300

    feat: create EvaluateNotebookEntityDownload entity

--- src/domain/entities/notebook/evaluate-notebook-entity.ts
+++ src/domain/entities/notebook/evaluate-notebook-entity.ts
@@ -4,3 +4,42 @@ export type EvaluateNotebookEntity = Omit<
   // eslint-disable-next-line prettier/prettier
   NotebookEntity, 'idcad' | 'studentName' | 'studentRegistration' | 'idpep' | 'reservationDate' | 'evaluatedDate' | 'notebookDirectory'
 >;
+export type EvaluateNotebookEntityDownload = {
+  'ID do caderno': number;
+  'ID do voluntário': number | string;
+  'Uni. prisional': string;
+  'ID da turma': number | string;
+  'Nome do voluntário': string;
+  'E-mail do voluntário': string;
+  'Matrícula do aluno': string;
+  'Nome do(a) aluno(a)': string;
+  'Carimbo de data/hora': Date | string;
+  'Data e hora da reserva': Date | string;
+  'Conteúdos relevantes': string;
+  'Avaliação da Questão 1:': string;
+  'Avaliação da Questão 2:': string;
+  'Avaliação da Questão 3:': string;
+  'Avaliação da Questão 4:': string;
+  'Avaliação da Questão 5:': string;
+  'Avaliação da Questão 6:': string;
+  'Avaliação da Questão 7:': string;
+  'Avaliação da Questão 8:': string;
+  'Avaliação da Questão 9:': string;
+  'Avaliação da Questão 10:': string;
+  'Avaliação da Questão 11:': string;
+  'Avaliação da Questão 12:': string;
+  'Avaliação da Questão 13:': string;
+  'Questão 1:': string;
+  'Questão 2:': string;
+  'Questão 3:': string;
+  'Questão 4:': string;
+  'Questão 5:': string;
+  'Questão 6:': string;
+  'Questão 7:': string;
+  'Questão 8:': string;
+  'Questão 9:': string;
+  'Questão 10:': string;
+  'Conclusão do avaliador': string;
+  Aprovado: string;
+  'Exclusão de arquivos recebidos': string;
+};
