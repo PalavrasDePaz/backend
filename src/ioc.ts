c8ba72f662c219f0315c4a53bbd6906ee68fbe23
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2025-01-15 19:12:10 -0300

    feat: implement InversifyAdapter for improved IoC container management

--- src/ioc.ts
+++ src/ioc.ts
@@ -1,11 +1,30 @@
 import { Container, decorate, injectable } from 'inversify';
 import { buildProviderModule } from 'inversify-binding-decorators';
 import { Controller } from 'tsoa';
+import { IocContainer } from 'tsoa';
+import { EmailManager } from './services/email-service/email-manager';
 
-const iocContainer = new Container();
+export class InversifyAdapter implements IocContainer {
+  private container: Container;
+
+  constructor(container: Container) {
+    this.container = container;
+  }
+
+  get<T>(controller: { prototype: T }): T {
+    const serviceIdentifier = controller as unknown as string;
+    return this.container.get<T>(serviceIdentifier);
+  }
+}
+
+const container = new Container();
 
 decorate(injectable(), Controller);
 
-iocContainer.load(buildProviderModule());
+container.load(buildProviderModule());
+
+console.log(container.isBound(EmailManager));
+
+const iocContainer = new InversifyAdapter(container);
 
 export { iocContainer };
