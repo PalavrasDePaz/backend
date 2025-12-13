c5f0218cf9da5414cbc576a458365a0ba7490998
Gonzagadavid <gonzagadaviddev@gmail.com>
2025-12-13 17:50:15 -0300

    feat: adjust type format

--- src/domain/entities/notebook/evaluate-notebook-entity.ts
+++ src/domain/entities/notebook/evaluate-notebook-entity.ts
@@ -2,14 +2,7 @@ import { NotebookEntity } from './notebook-entity';
 
 export type EvaluateNotebookEntity = Omit<
   // eslint-disable-next-line prettier/prettier
-  NotebookEntity,
-  | 'idcad'
-  | 'studentName'
-  | 'studentRegistration'
-  | 'idpep'
-  | 'reservationDate'
-  | 'evaluatedDate'
-  | 'notebookDirectory'
+  NotebookEntity, 'idcad' | 'studentName' | 'studentRegistration' | 'idpep' | 'reservationDate' | 'evaluatedDate' | 'notebookDirectory'
 >;
 export type EvaluateNotebookEntityDownload = {
   'ID do caderno': number;
