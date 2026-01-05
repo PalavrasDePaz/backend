e7bd851429579da843bafc836995b3a239a1a053
Victor Seiji Hariki <victorseijih@gmail.com>
2026-01-05 14:21:35 -0300

    fix: fix injecting email from outside
    Signed-off-by: Victor Seiji Hariki <victorseijih@gmail.com>

--- src/domain/errors/send-email.ts
+++ src/domain/errors/send-email.ts
@@ -1,5 +1,5 @@
 import { ErrorBase } from './error-base';
 
-type ErrorName = 'COULD_NOT_SEND_EMAIL_ERROR';
+type ErrorName = 'COULD_NOT_SEND_EMAIL_ERROR' | 'COULD_NOT_INJECT_EMAIL_ERROR';
 
 export class SendEmailError extends ErrorBase<ErrorName> {}
